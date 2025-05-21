b1a7ee417641b9ab939e4ba4e75628503770bd34
AlsinaBautista <balsina@udesa.edu.ar>
2025-05-21 13:49:44 -0300

    Empece a hacer la clase tower

--- juego.py
+++ juego.py
@@ -2,16 +2,23 @@ import pygame
 import constantes as c
 from map import Map
 from enemy import Enemy
+from tower import Tower
 
 pygame.init()
 
 screen = pygame.display.set_mode((c.width, c.height))
-icono = pygame.image.load("icono_globos.jpeg")
+icono = pygame.image.load("TP_Final/icono_globos.jpeg")
 pygame.display.set_icon(icono)
 pygame.display.set_caption("Bloons TD")
 
-map = Map("fondo.png")
+map = Map("TP_Final/fondo.png")
 enemy = Enemy(c.enemy_pos, c.enemy_speed, c.enemy_health, c.enemy_img, c.enemy_path)
+
+# Imagen de torre dummy (puede ser reemplazada luego)
+tower_img = pygame.image.load("TP_Final/tower.png")
+
+# Crear una torre de prueba en el centro de la pantalla
+test_tower = Tower(pos=(c.width//2, c.height//2), scope=100, damage=1, att_speed=1, target=None, price=50, image=tower_img)
 
 run = True
 while run:
@@ -25,6 +32,11 @@ while run:
 
     enemy.move()
     enemy.draw(screen)
+
+    test_tower.draw_scope(screen)  # Esto dibuja el círculo de alcance
+    screen.blit(test_tower.img, (test_tower.pos[0] - test_tower.img.get_width()//2,
+                                test_tower.pos[1] - test_tower.img.get_height()//2))
+    print(test_tower.enemies_in_range(enemy.pos))
 
     pygame.display.update()
 
--- tower.py
+++ tower.py
@@ -1,11 +1,32 @@
+import pygame
+import constantes as c
+
 class Tower:
 
-    def __init__(self, pos, rarity, scope, damage, att_speed, target):
+    def __init__(self, pos, scope, damage, att_speed, target, price, image):
         self.pos = pos
-        self.rarity = rarity
         self.scope = scope
         self.damage = damage
         self.att_speed = att_speed
         self.target = target
+        self.price = price
+        self.img = image
+    
+    def set_tower(self, x, y):
+        self.pos = (x, y)
+    
+    def draw_scope(self, screen):
+        pygame.draw.circle(screen, c.white, self.pos ,self.scope, 1)
+
+    def enemies_in_range(self, pos_enemy):
+        xe, ye = pos_enemy
+        xt, yt = self.pos
+        if (xt - self.scope) <= xe <= (xt + self.scope):
+            if (yt - self.scope) <= ye <= (yt + self.scope):
+                return True
+        return False
+    
+
+
 
     
