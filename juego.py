311257a2bca8be40e2fbcc52fea1d3bfdaf3de23
AlsinaBautista <balsina@udesa.edu.ar>
2025-05-29 16:55:04 -0300

    Ahora se dibujan las celdas cuando estas colocando una torre

--- juego.py
+++ juego.py
@@ -157,6 +157,7 @@ while run:
         screen.blit(scope_surface, (mouse_pos[0] - dragging_tower.scope, mouse_pos[1] - dragging_tower.scope))
         dragging_tower.set_tower(*mouse_pos) # Actualiza la posicion de la torre arrastrada
         screen.blit(dragging_tower.img, (mouse_pos[0] - dragging_tower.img.get_width()//2, mouse_pos[1] - dragging_tower.img.get_height()//2)) # Dibuja la imagen de la torre en la pantalla, de forma que su centro este exactamente donde esta el mouse
+        map.draw_celds(screen, c.WHITE, c.CELD)
 
     if life.cant_total <= 0:
         font = pygame.font.Font('fonts/OETZTYP_.TTF', 48)
